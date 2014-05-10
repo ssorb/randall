d3bb151192f482bc5066cfeba36fbbcc0d451636
Wilkes Joiner <wilkesjoiner@gmail.com>
2014-05-10 10:04:31 -0500

    initial version w/ figwheel

---
+++ project.clj
@@ -0,0 +1,27 @@
+(defproject randall "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/clojurescript "0.0-2197"]
+                 [figwheel "0.1.1"]
+                 [om "0.6.2"]]
+
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-figwheel "0.1.1"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild { 
+    :builds [{:id "randall"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "resources/public/js/compiled/randall.js"
+                :output-dir "resources/public/js/compiled/out"
+                :optimizations :none
+                :source-map true}}]}
+  :figwheel {:http-server-root "public" ;; default and assumes "resources"
+             :server-port 3449 ;; default
+             :css-dirs ["public/resources/css"] ;; watch and update CSS
+             })
---
+++ src/randall/core.cljs
@@ -0,0 +1,18 @@
+(ns randall.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [figwheel.client :as fw :include-macros true]))
+
+(enable-console-print!)
+
+(def app-state (atom {:text "Hello bomb!"}))
+
+(om/root
+  (fn [app owner]
+    (dom/h1 nil (:text app)))
+  app-state
+  {:target (. js/document (getElementById "app"))})
+
+(fw/watch-and-reload
+  ;; :websocket-url "ws:localhost:3449/figwheel-ws" default
+  :jsload-callback (fn [] (print "reloaded"))) ;; optional callback
